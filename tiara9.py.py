11e8881ad268183168bb475d35cfdb183e1c1c69
Tiaradekantari <tiarafaaa@gmail.com>
2024-12-15 16:08:16 +0800

    Add files via upload

---
+++ tiara9.py.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 32
+# suhu dalam fahrenheit = 89.6
+# suhu dalam kelvin = 305.15
+# suhu dalam reamur = 25.6
+
+# suhu dalam celcius
+celcius = 32
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
